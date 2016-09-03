47963dac01fc1c4ed40ca26e5e4d4f39182f1ff8
Daniel Gauci <frikxen@gmail.com>
2016-09-03 18:56:00 +1000

    JSON Reader
    Added package "Newtonsoft.Json" to read and write JSON files.
    Added new class "Product"
    Added method "LoadProduct" method to "DatabaseManager"

--- TeamAmcal/TeamAmcal/DatabaseManager.cs
+++ TeamAmcal/TeamAmcal/DatabaseManager.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace TeamAmcal
 {
     class DatabaseManager
     {
+        private List<Product> productList = new List<Product>();
+
+        public void LoadProducts()
+        {
+            using (StreamReader file = File.OpenText(Directory.GetCurrentDirectory() + "\\" + "Database.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                Product p = (Product)serializer.Deserialize(file, typeof(Product));
+                productList.Add(p);
+            }
+        }
     }
 }
