a114986810e76e322b571eeb9fd4fed7d97d8095
100016670 <ramanmalhotra_27@yahoo.com>
2016-09-05 16:10:54 +1000

    convert-csv
    created a method to convert a text file to csv

---
+++ TeamAmcal/TeamAmcal/converttocsv.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamAmcal
+{
+    class converttocsv
+    {
+        public void texttocsv()
+        {
+            string[] lines = System.IO.File.ReadAllLines("YOUR INPUT FILE");
+            StringBuilder builder = new StringBuilder();
+            foreach (string line in lines)
+            {
+                var temp = line.Split('\t');
+                builder.AppendLine(string.Join(",", temp[0], temp[1]));
+                //builder.AppendLine(string.Format("{0}; {1}", temp[0], temp[1]));
+            }
+
+            System.IO.File.WriteAllText("", builder.ToString());
+        }
+
+    }
+}
