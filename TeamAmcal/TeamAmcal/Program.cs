59009d53479cb815fd14935db06ca84d4fcf3336
Jakeamorris25 <Jakeamorris25@gmail.com>
2016-09-06 13:03:16 +1000

    Added method to add product sales
    Added product sales method that can: Add sales, Read saved sales, add
    the total of all sales for a product

--- TeamAmcal/TeamAmcal/Program.cs
+++ TeamAmcal/TeamAmcal/Program.cs
@@ -14,9 +14,19 @@ namespace TeamAmcal
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+
+
+            //Testing
+            Sales s = new Sales();
+            //s.AddSalesData("testProduct", "testDate", 10, 10, 10, 10);
+
+            for (int i = 0; i < 10; i++)
+                s.AddSalesData("testProduct", "testDate2", 5, 5, 5, 5);
+
+            //s.AddTotals("testKey");
         }
     }
 }
