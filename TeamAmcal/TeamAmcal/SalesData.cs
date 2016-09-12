550d6d7cca8491f347efaa075309d5ae9b794814
100054421 <100054421@student.swin.edu.au>
2016-09-12 16:25:21 +1000

    Added-Jake's-changes
    Added the changes that Jake made

---
+++ TeamAmcal/TeamAmcal/SalesData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamAmcal
+{
+    class SalesData
+    {
+        public SalesData(string rDate, int rQuantity, float rPrice, float rDiscounted, float rTotal)
+        {
+            Date = rDate;
+            Quantity = rQuantity;
+            Price = rPrice;
+            Discounted = rDiscounted;
+            Total = rTotal;
+        }
+
+        public int Key { get; set; }
+        public string Date { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public float Discounted { get; set; }
+        public float Total { get; set; }
+    }
+}
