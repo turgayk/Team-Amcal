0db387be2bc56aefe75b7f6279697619018723ee
100016670 <ramanmalhotra_27@yahoo.com>
2016-09-29 15:33:56 +1000

    sxd

--- TeamAmcal/TeamAmcal/accessingtextfile.cs
+++ TeamAmcal/TeamAmcal/accessingtextfile.cs
@@ -8,21 +8,6 @@ namespace TeamAmcal
 {
     class accessingtextfile
     {
-        public void texttocsv()
-        {
-            string[] lines = System.IO.File.ReadAllLines("YOUR INPUT FILE");
-            StringBuilder builder = new StringBuilder();
-            foreach (string line in lines)
-            {
-                var temp = line.Split('\t');
-                builder.AppendLine(string.Join(",", temp[0], temp[1]));
-                //builder.AppendLine(string.Format("{0}; {1}", temp[0], temp[1]));
-            }
-
-            System.IO.File.WriteAllText("", builder.ToString());
-
-        }
-
         static void Main()
         {
             // Read each line of the file into a string array. Each element
