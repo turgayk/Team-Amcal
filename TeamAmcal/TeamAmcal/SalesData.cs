33997c514156cb6626fa2aa8cdc57e3f618868b9
Jakeamorris25 <Jakeamorris25@gmail.com>
2016-09-29 15:56:18 +1000

    Fixes

---
+++ TeamAmcal/TeamAmcal/SalesData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamAmcal
+{
+    class SalesData
+    {
+        public SalesData(string rDate, int rQuantity, float rPrice, float rDiscounted, float rSoldFor)
+        {
+            Date = rDate;
+            Quantity = rQuantity;
+            Price = rPrice;
+            Discounted = rDiscounted;
+            SoldFor = rSoldFor;
+        }
+
+        public int Key { get; set; }
+        public string Date { get; set; }
+        public int Quantity { get; set; }
+        public float Price { get; set; }
+        public float Discounted { get; set; }
+        public float SoldFor { get; set; }
+    }
+}
